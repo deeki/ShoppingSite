bfc46a1f098e6b94d09ac18df8040fd189e8c2af
Deeksha Gupta <deekshagupta0207@gmail.com>
2020-09-14 20:01:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+      <div className="grid-container">
+            <header>
+                <nav>
+                  <a href="/">Shopping site</a>
+                </nav>
+            </header>
+            <main>This is main</main>
+            <footer>This is footer</footer>
+      </div>
+  );
+}
+
+export default App;
